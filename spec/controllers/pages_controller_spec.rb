26bf05c7a7d198f8dd2d2495ae13b4a7d6213806
Deanish <m.a.deanish@gmail.com>
2015-01-22 13:13:11 +0530

    Added a site layout

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,16 +2,31 @@ require 'rails_helper'
 
 RSpec.describe PagesController, :type => :controller do
 
+  render_views
+
   describe "GET home" do
     it "returns http success" do
       get :home
       expect(response).to have_http_status(:success)
     end
+
+    # it "should have the right title" do
+    #   get :home
+    #   response.should have_selector("title", :content => "Ruby on Rails | Home")
+    # end
+
   end
 
   describe "GET contact" do
     it "returns http success" do
       get :contact
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET about" do
+    it "returns http success" do
+      get :about
       expect(response).to have_http_status(:success)
     end
   end
